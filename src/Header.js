af38afd4da4135863c2ef9c239d63ec7b31abaf6
Andrew D'Ambrosio <adambrosio20@gmail.com>
2020-10-28 17:17:27 -0400

    added test header component

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            <h1>H1 Header</h1>
+            <h2>H2</h2>
+            <ol>
+                <li>Item 1</li>
+                <li>Item 2</li>
+            </ol>
+        </div>
+    );
+}
+
+export default Header;
